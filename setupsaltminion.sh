d994293ec2bc784d5cc8768183fc689b228b9a52
GA <ga_acad@yahoo.com>
2017-02-16 14:30:33 +0530

    .

--- setupsaltminion.sh
+++ setupsaltminion.sh
@@ -2,3 +2,10 @@
 
 sudo apt-get install -y salt-minion
 
+echo "Enter DNS/IP of Salt-Master:"
+read saltmaster_ip
+
+sudo sed -i "16s/.*/master: $saltmaster_ip/" /etc/salt/minion
+grep master /etc/salt/minion
+
+
